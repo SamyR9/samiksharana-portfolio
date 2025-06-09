44a0e81ce2facc5b284a1a374abaf751bdd5dea0
Samiksha Rana <28333690+SamyR9@users.noreply.github.com>
2025-06-09 22:12:40 +0530

    base path fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: "/samiksharana-portfolio/public",
+  basePath: "/samiksharana-portfolio",
   output: "export"
 };
 
